762fcc5a20eab154d0b808e5643352aea756ff85
Shriya Goyal <shriya@student.unimelb.edu.au>
2023-06-01 02:46:11 +1000

    changes

---
+++ Version2/script.js
@@ -0,0 +1,33 @@
+const smallImgRow = document.querySelector('.small-img-row');
+const smallImgCols = document.querySelectorAll('.small-img-col');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+let currentIndex = 0;
+
+arrowLeft.addEventListener('click', () => {
+    if (currentIndex > 0) {
+    currentIndex--;
+    smallImgRow.classList.add('slide-effect');
+    smallImgRow.scrollBy({
+        left: -smallImgCols[0].offsetWidth,
+        behavior: 'smooth'
+    });
+    }
+});
+
+arrowRight.addEventListener('click', () => {
+    if (currentIndex < smallImgCols.length - 1) {
+    currentIndex++;
+    smallImgRow.classList.add('slide-effect');
+    smallImgRow.scrollBy({
+        left: smallImgCols[0].offsetWidth,
+        behavior: 'smooth'
+    });
+    }
+});
+
+smallImgRow.addEventListener('scroll', () => {
+    smallImgRow.classList.remove('slide-effect');
+    currentIndex = Math.round(smallImgRow.scrollLeft / smallImgCols[0].offsetWidth);
+});
+  
